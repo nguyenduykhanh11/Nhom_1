f2ae62d57bf0e56642b800de1e369d5af1b4dff4
Nguyễn Duy Khánh <you@example.com>
2022-11-14 15:22:13 +0700

    CSS and Clear

--- Front-end/html/js/config.js
+++ Front-end/html/js/config.js
@@ -1,6 +1,6 @@
 const Config = {
-    nftAddress: "0x7b5Bc18B12c23974Cd4bA056a416a44e1a78a487",
-    nftAbi: [
+    GiaiThuaAddress: "0x7b5Bc18B12c23974Cd4bA056a416a44e1a78a487",
+    GiaiThuaAbi: [
         
             {
 				"inputs": [
--- Front-end/html/js/index.js
+++
@@ -1,8 +0,0 @@
-function getContractInstance (abi, address) {
-    let contract = new web3.eth.Contract(abi, address);
-    return contract;
-}
-
-async function GiaiThua (instance, number) {
-    return await instance.methods.isGiaiThua(number).call();
-}
